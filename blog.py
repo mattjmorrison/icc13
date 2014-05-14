9ff6ccb0c30c5d3ddc0ea52028c42cc336499943
mattjmorrison <mattjmorrison@mattjmorrison.com>
2014-05-14 08:20:21 -0500

    RESET

--- blog.py
+++
@@ -1,9 +0,0 @@
-class Blog(object):
-
-    def __init__(self, persistence):
-        self.persistence = persistence
-        self.posts = self.persistence.get()
-
-    def create_post(self, title):
-        self.posts.append(title)
-        self.persistence.put(self.posts)
